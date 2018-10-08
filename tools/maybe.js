d1892b5e32eeea2cf13ff10500fd1a1364b10930
James Dean <james@terradomain.us>
2018-10-08 10:23:29 -0500

    Adds a `match` helper for maybe-like arrays.
    It emulates pattern-matching for `maybe` objects and arrays.

--- tools/maybe.js
+++ tools/maybe.js
@@ -48,4 +48,23 @@ export function map(xformFn) {
   return this.length > 0 ? Object.freeze(this.map(xformFn)) : maybe.nothing;
 }
 
+/**
+ * A helper for simple pattern-matching.  If `expr` evaluates to an array, it will be applied to `xformFn`.
+ * If the result of that application is a non-empty array, it will be the result of the match.  This function
+ * can be chained with the bind-operator `::`, and the bound object will be returned if it is not an empty-array.
+ *
+ * @export
+ * @template T,U
+ * @param {Array<U>} this The bound object, which should be an array.
+ * @param {Array<T>} expr An `Array` with at least one element; otherwise the match fails.
+ * @param {(option: Array<T>) => Array<U>} xformFn A function to call in the case of a match; should return a `maybe`.
+ * @returns {Array<U>} May be an object that was matched.
+ */
+export function match(expr, xformFn) {
+  'use strict';
+  if (Array.isArray(this) && this.length > 0) return this;
+  if (Array.isArray(expr) && expr.length > 0) return xformFn(expr) ?? maybe.nothing;
+  return maybe.nothing;
+}
+
 export default maybe;
