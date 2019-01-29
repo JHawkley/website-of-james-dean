457616ae3408166a53e0a69c451b5adc28a8ec0c
James Dean <james@terradomain.us>
2019-01-29 14:15:46 -0600

    Changes to the polyfills.
    This should have been committed first earlier.  Oh well...

--- patch/client-polyfills.js
+++ patch/client-polyfills.js
@@ -1,5 +1,5 @@
 import 'core-js/es6/symbol';
+import 'core-js/es7/symbol';
+import 'core-js/es6/array/';
+import 'core-js/es7/array';
 import 'core-js/web/immediate';
-import 'core-js/modules/es7.symbol.async-iterator';
-import 'core-js/modules/es7.array.includes';
-import 'core-js/modules/es7.promise.finally';
