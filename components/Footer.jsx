feaa57a817faa3f038fcd4a7c2865a804a3eba2f
James Dean <james@terradomain.us>
2019-02-13 13:27:20 -0600

    Refactor to `components/Footer` and related styles.
    Now accepts a `children` attribute.
    Change how the transitions are done in the static CSS file.
    My personal modifications to the contents and style are now intended to be provided elsewhere (in the index page itself).
    Later commits will reflect these changes in other locations.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,10 @@
 import PropTypes from "prop-types";
-import Jump from "./Jump";
 
-const Footer = ({className}) => (
-  <footer id="footer" className={className}>
-    <p className="copyright">Template: &copy; <Jump href="https://github.com/codebushi/nextjs-starter-dimension" icon="none">Next.js Starter - Dimension</Jump>.  Content: &copy; James Dean</p>
-    <p className="copyright">Design: <Jump href="https://html5up.net" icon="none">HTML5 UP</Jump>.  Built with: <Jump href="https://github.com/zeit/next.js" icon="none">Next.js</Jump></p>
-  </footer>
-);
+const Footer = ({children, className}) =>
+  <footer id="footer" className={className}>{children}</footer>;
 
 Footer.propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string
 };
 
