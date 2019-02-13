7c38f59d79405a3647b690bd5a7728551daac19f
James Dean <james@terradomain.us>
2019-02-13 14:00:51 -0600

    Refactor to `pages/_document`.
    Now handles the `<noscript />` portion of the app.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,40 @@
+import { Fragment } from "react";
+import ReactDOMServer from "react-dom/server";
+import flushStyles from "styled-jsx/server";
 import Document, { Head, Main, NextScript } from "next/document";
 import siteStyle from "styles/main.scss";
 import lightboxStyle from "react-image-lightbox/style.css";
 import { dom as faDom } from "@fortawesome/fontawesome-svg-core";
 
+import AppRoot from "components/AppRoot";
+import Wrapper from "components/Wrapper";
+import Page from "components/Page";
+import NoScriptPage from "pages/_noscript";
 
 export default class StyledDocument extends Document {
+
+  renderNoScript() {
+    // Based on: https://github.com/facebook/react/issues/11423#issuecomment-341760646
+    const { render, exitDelay } = { ...Page.transition, ...NoScriptPage.transition };
+
+    const app = ReactDOMServer.renderToString(
+      <AppRoot>
+        <Wrapper>
+          {render(NoScriptPage, {}, exitDelay, "entered")}
+        </Wrapper>
+        <style jsx global>{`#__next { display: none !important; }`}</style>
+      </AppRoot>
+    );
+
+    const staticMarkup = ReactDOMServer.renderToStaticMarkup(
+      <Fragment>
+        { flushStyles() }
+        <div id="__noscript" dangerouslySetInnerHTML={{__html: app}} />
+      </Fragment>
+    );
+
+    return <noscript dangerouslySetInnerHTML={{ __html: staticMarkup }} />;
+  }
 
   render() {
     return (
@@ -16,6 +46,7 @@ export default class StyledDocument extends Document {
           <style dangerouslySetInnerHTML={{ __html: faDom.css() }} />
         </Head>
         <body>
+          { this.renderNoScript() }
           <Main />
           <NextScript />
         </body>
