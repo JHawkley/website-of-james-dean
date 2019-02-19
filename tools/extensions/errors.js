7c31bea74a166254aec8e6b72abc687911bb3ad3
James Dean <james@terradomain.us>
2019-02-19 17:18:45 -0600

    Adds `tools/errors` and extensions.
    Provides extensions for iterating over errors with an `innerError` property.

---
+++ tools/extensions/errors.js
@@ -0,0 +1,128 @@
+import { is } from "tools/common";
+import BadArgumentError from "lib/BadArgumentError";
+
+/**
+ * Converts an object to an error.
+ * If the object is already an error, it simply returns it.
+ * Otherwise, `toString` is called on the object, and that is used as the error's message.
+ * 
+ * @export
+ * @this {*} This object.
+ * @returns {Error}
+ */
+export function asError() {
+  if (this::is.error()) return this;
+  if (this::is.string()) return new Error(this);
+  return new Error(this.toString());
+}
+
+/**
+ * Traverses this error, including each `innerError`, and applies the given function.
+ *
+ * @export
+ * @this {Error} This error.
+ * @param {ForEachFunction} fn The function to be applied to each error.
+ * @throws When `fn` is not a function.
+ */
+export function foreach(fn) {
+  const originalError = this::asError();
+
+  if (!fn::is.func())
+    throw new BadArgumentError("must be a function", "fn", fn);
+
+  let error = originalError;
+  let depth = 0;
+  while (error != null) {
+    fn(error, depth, originalError);
+    error = error.innerError::asError();
+    depth = depth + 1;
+  }
+}
+
+/**
+ * Traverses this error, including each `innerError`, and transforms each one with the given `transformationFn`.
+ *
+ * @export
+ * @template T
+ * @this {Error} This error.
+ * @param {TransformationFunction<T>} transformationFn The transformation function.
+ * @returns {T[]} The transformed results.
+ * @throws When `transformationFn` is not a function.
+ */
+export function map(transformationFn) {
+  const originalError = this::asError();
+
+  if (!transformationFn::is.func())
+    throw new BadArgumentError("must be a function", "transformationFn", transformationFn);
+  
+  const mappedElements = [];
+
+  let error = originalError;
+  let depth = 0;
+  while (error != null) {
+    const result = transformationFn(error, depth, originalError);
+    mappedElements.push(result);
+    error = error.innerError::asError();
+    depth = depth + 1;
+  }
+
+  return mappedElements;
+}
+
+/**
+ * Traverses this error, including each `innerError`, and applies the given partial-function to each,
+ * discarding any result that is `undefined`.
+ *
+ * @export
+ * @template T
+ * @this {Error} This error.
+ * @param {PartialFunction<T>} partialFn The partial function.
+ * @returns {T[]} The collected results.
+ * @throws When `partialFn` is not a function.
+ */
+export function collect(partialFn) {
+  const originalError = this::asError();
+
+  if (!partialFn::is.func())
+    throw new BadArgumentError("must be a function", "partialFn", partialFn);
+  
+  const collectedElements = [];
+
+  let error = originalError;
+  let depth = 0;
+  while (error != null) {
+    const result = partialFn(error, depth, originalError);
+    if (!result::is.undefined())
+      collectedElements.push(result);
+    error = error.innerError::asError();
+    depth = depth + 1;
+  }
+
+  return collectedElements;
+}
+
+/**
+ * @callback ForEachFunction
+ * @param {Error} error The current error.
+ * @param {number} depth The depth traversed so far.
+ * @param {Error} originalError The original error being traversed.
+ * @returns {void}
+ */
+
+/**
+ * @template T
+ * @callback TransformationFunction
+ * @param {Error} error The current error.
+ * @param {number} depth The depth traversed so far.
+ * @param {Error} originalError The original error being traversed.
+ * @returns {T} A new value.
+ */
+
+/**
+ * @template T
+ * @callback PartialFunction
+ * @param {Error} error The current error.
+ * @param {number} depth The depth traversed so far.
+ * @param {Error} originalError The original error being traversed.
+ * @returns {T|void} A new value or `undefined` in the case the function could not be applied to the error.
+ */
