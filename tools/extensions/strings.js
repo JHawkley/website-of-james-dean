b0434e91c33014effb6ad19e80f992c12fb80e4f
James Dean <james@terradomain.us>
2019-02-19 19:54:09 -0600

    Adds `tools/extensions/string.orNull`; other refactors.

--- tools/extensions/strings.js
+++ tools/extensions/strings.js
@@ -1,18 +1,33 @@
+import { is } from "tools/common";
 import BadBindingError from "lib/BadBindingError";
 
 /**
- * Determines if this string is `null` or the empty-string.
+ * Determines if this string is `null`, `undefined`, or the empty-string.
  * 
  * @export
- * @this {string} This string.
- * @returns {boolean} Whether this string is `null` or empty-string.
+ * @this {?string} This string.
+ * @returns {boolean} Whether this string is `null`, `undefined`, or empty-string.
  */
 export function isNullishOrEmpty() {
   'use strict'; // Allows binding to `null`.
 
   if (this == null) return true;
   if (this === "") return true;
-  if (typeof this !== "string")
-    throw new BadBindingError(`expected a string`, this);
-  return false;
+  if (this::is.string()) return false;
+  throw new BadBindingError(`expected a string`, this);
+}
+
+/**
+ * Converts `null`, `undefined`, and the empty-string into `null`, but leaves any other string as-is.
+ *
+ * @export
+ * @this {?string} This string.
+ * @returns {?string}
+ */
+export function orNull() {
+  'use strict'; // Allows binding to `null`.
+  if (this == null) return null;
+  if (this === "") return null;
+  if (this::is.string()) return this;
+  throw new BadBindingError(`expected a string`, this);
 }
