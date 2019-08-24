9bb4fbbf6251e30a7582dcb5d3fd1d3c73c6ad74
James Dean <james@terradomain.us>
2019-08-24 17:26:20 -0500

    Documentation correction.

--- tools/parsing/modifiers/flatten.js
+++ tools/parsing/modifiers/flatten.js
@@ -5,7 +5,7 @@ import { flattenBy } from "./flattenBy";
  *
  * @export
  * @template T
- * @param {Parser<Array<T|T[]>> parser The parser whose result will be flattened.
+ * @param {Parser<Array<T|T[]>>} parser The parser whose result will be flattened.
  * @returns {Parser<T[]>} A parser that produces an array.
  */
 export const flatten = (parser) => flattenBy(parser, 1);
