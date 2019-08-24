681f3a1fde842295ffa8b1c27d0f2e3036a67fa1
James Dean <james@terradomain.us>
2019-08-24 17:46:48 -0500

    Updates to `PreloadSync`
    Now has a concept of "important" sources.
    These sources are considered required for some feature before that feature can run.
    In the future, a special component can wait for all important sources to finish before it will attempt to render a feature.
    Also adds an event system to make listening for updates to the state of preloading easier.
    Some minor API changes to mirror `tools/async/Stream`.

--- components/Preloader/PreloadSync.js
+++ components/Preloader/PreloadSync.js
@@ -1,7 +1,13 @@
+import mitt from "next-server/dist/lib/mitt";
 import { is } from "tools/common";
 import { Stream, debounce } from "tools/async";
 import { extensions as asyncIterEx } from "tools/asyncIterables";
 import PreloadError from "components/Preloader/PreloadError";
+
+const $completed = "completed";
+const $done = "done";
+const $failed = "failed";
+const $hasImportantSourcesChanged = "hasImportantSourcesChanged";
 
 const $$fresh = Symbol("preload-sync:fresh");
 const $$preloading = Symbol("preload-sync:preloading");
@@ -15,34 +21,73 @@ class PreloadSync {
     preloaded: $$preloaded
   };
 
-  errorHandler = null;
+  events = mitt();
+
   preloading = new Set();
+  importantOnes = new Set();
+
+  state = $$fresh;
   stream = new Stream();
   updates = this.stream::asyncIterEx.fromLatest();
-  state = $$fresh;
+
+  get isCompleted() { return this.stream.isCompleted; }
+  get didError() { return this.stream.didError; }
+  get isDone() { return this.isCompleted && !this.didError; }
 
   constructor(errorHandler) {
     this.errorHandler = errorHandler;
   }
 
+  // Public methods.
+
   rendered = debounce(() => {
-    if (this.stream.isCompleted) return;
+    if (this.isCompleted) return;
     if (this.state !== $$fresh) return;
     this.setState($$preloaded);
   });
 
   done() {
-    if (this.stream.isCompleted) return;
+    if (this.isCompleted) return;
     this.stream.done();
-    this.preloading.clear();
+    this.handleTearDown(true);
   }
 
-  update(source, preloaded, error) {
-    if (this.stream.isCompleted) return;
+  update(source, preloaded, error, important = false) {
+    if (this.isCompleted) return;
 
     if (error) this.handleError(source, error);
-    else if (!preloaded) this.mount(source);
+    else if (!preloaded) this.mount(source, important);
     else this.dismount(source);
+  }
+
+  // Private methods.
+
+  handleTearDown(fromDone) {
+    this.preloading.clear();
+    this.importantOnes.clear();
+
+    if (fromDone) this.events.emit($done);
+    else this.events.emit($failed);
+
+    this.events.emit($completed);
+  }
+
+  handleImportantSource(source, adding) {
+    const hasSource = this.importantOnes.has(source);
+
+    if (adding) {
+      if (hasSource) return;
+      const formerSize = this.importantOnes.size;
+      this.importantOnes.add(source);
+      if (formerSize === 0)
+        this.events.emit($hasImportantSourcesChanged, true);
+    }
+    else {
+      if (!hasSource) return;
+      this.importantOnes.delete(source);
+      if (this.importantOnes.size === 0)
+        this.events.emit($hasImportantSourcesChanged, false);
+    }
   }
 
   handleError(source, error) {
@@ -54,16 +99,19 @@ class PreloadSync {
     }
     else {
       this.stream.fail(error);
-      this.preloading.clear();
+      this.handleTearDown(false);
     }
   }
 
-  mount(source) {
+  mount(source, important) {
     this.preloading.add(source);
+    this.handleImportantSource(source, important);
     this.setState($$preloading);
   }
 
   dismount(source) {
+    this.handleImportantSource(source, false);
+
     if (this.preloading.has(source)) {
       this.preloading.delete(source);
       if (this.preloading.size === 0)
