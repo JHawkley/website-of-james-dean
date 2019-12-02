5b0eb968dff1636ca853d365628232145af31930
James Dean <1125102+JHawkley@users.noreply.github.com>
2019-12-02 08:18:28 -0800

    Changes to `intro` page; adds resume link.

--- pages/intro.jsx
+++ pages/intro.jsx
@@ -11,14 +11,14 @@ const IntroPage = (props) => (
       <ImgJamesPhoto alt="A Photo of James Dean" fluid />
     </span>
     <p>Welcome!  I'm James!  I'm a software developer in the greater Seattle area of Washington, USA.  I'm originally from Texas.</p>
-    <p>I have been working professionally since 2002.  In that time, I've put together and maintained three big applications for my former employer.  You can read all about it in the <ToWork>work section of the site</ToWork>.</p>
+    <p>I have been working professionally since 2003.  In that time, I've put together and maintained three big applications for my former employer.  You can read all about it in the <ToWork>work section of the site</ToWork>.</p>
     <p>My first serious programming was in C#, which I primarily used until 2010, when I decided to shift much of my focus on to JavaScript and related technologies.  I consider web services and applications to pretty much be the future of end-user software, a universal platform that spans all devices across the planet.</p>
     <p>While it probably won't realize that potential for a while longer, it will some day and in some form; I want to be a part of making it happen.</p>
-    <p>I primarily consider myself a front-end and application developer; it's certainly where I've had most of my success.  User experiences that I design tend to be highly contextual and minimally cluttered.  If it isn't important to the user at the time, it should be given little to no screen space.  I also like to keep things clear, so one part of the UI is easily distinguishable from another part.</p>
+    <p>I primarily consider myself a front-end developer with most of my experience being in desktop applications; it's certainly where I've had most of my success. User experiences that I design tend to be highly contextual and minimally cluttered. If it isn't important to the user at the time, it should be given little to no screen space. I also like to keep things clear, so one part of the UI is easily distinguishable from another part.</p>
     <p>I've also always had the dream to create games since I was in high school, which is where most of my spare time goes.  I have mostly been studying the technical aspects of game development, rather than building complete games.  The systems and algorithms that drive games fascinate me.</p>
     <p>I just have not yet gotten together with like-minded individuals who can handle the more artistic side of things and tough it out with me until the end.</p>
     <p>If you are such a person, please have a look at my <ToWork>game-specific works here</ToWork>, toward the bottom of the page, to help you determine if I might be an asset to your project.</p>
-    <p>A table of my skills is below:</p>
+    <p>For recruiters and those involved with talent acquisition, you may wish to view <a href="/static/misc/James%20Dean%20-%20Resume.pdf">my current resume</a> and the table of my skills below:</p>
     <table>
       <thead>
         <tr>
